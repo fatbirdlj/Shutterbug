1009f52bf804cc84210396e93ef35cff755e2d17
Flicker <fatbirdlj@gmail.com>
2017-04-06 14:59:07 +0800

    First draft for shutterbug

---
+++ Shutterbug/Flickr Fetcher/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2013.
+//  Copyright 2013 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"d18c34ea5fc7cd71ea3efd5b4c5a9811"
